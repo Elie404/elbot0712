a5a833594c084c0c641c9be2f7343977968f54e2
Elie404 <79168733+Elie404@users.noreply.github.com>
2021-05-03 12:35:10 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -13,7 +13,7 @@ client.on("ready", () => {
     `Oh tiens je m'appelle ${client.user.tag} (ID : ${client.user.id})`
   );
   {
-    client.user.setActivity("Chante tutititutu tout en changeant pour ubuntu");
+    bot.change_presence(activity=discord.Streaming(name="Chante tutititutu tout en changeant pour ubuntu!", url="https://www.twitch.tv/eli__zay"));
   }
   // Auto Up
   setInterval(() => {
