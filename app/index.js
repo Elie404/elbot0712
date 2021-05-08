8fb10acb14e557e5690b9b59f83f164b1e6c001b
superbar13 <64144986+superbar13@users.noreply.github.com>
2021-05-08 16:32:28 +0200

    Update index.js

--- app/index.js
+++ app/index.js
@@ -34,97 +34,63 @@ client.on("ready", () => {
 });
 
 async function execute(message, serverQueue) {
-  const args = message.content.split(" "); // On récupère les arguments dans le message pour la suite
+        const args = message.content.split(" "); // On récupère les arguments dans le message pour la suite
 
-  const voiceChannel = message.member.voice.channel;
-  if (!voiceChannel) // Si l'utilisateur n'est pas dans un salon vocal
-    return message.channel.send(
-      "Vous devez être dans un salon vocal!"
-    );
-  const permissions = voiceChannel.permissionsFor(message.client.user); // On récupère les permissions du bot pour le salon vocal
-  if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) { // Si le bot n'a pas les permissions
-    return message.channel.send(
-      "J'ai besoin des permissions pour rejoindre le salon et pour y jouer de la musique!"
-    );
-  }
+        const voiceChannel = message.member.voice.channel;
+        if (!voiceChannel) // Si l'utilisateur n'est pas dans un salon vocal
+        {
+                return message.channel.send(
+                    "Vous devez être dans un salon vocal!"
+                );
+        }
+        const permissions = voiceChannel.permissionsFor(message.client.user); // On récupère les permissions du bot pour le salon vocal
+        if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) { // Si le bot n'a pas les permissions
+                return message.channel.send(
+                    "J'ai besoin des permissions pour rejoindre le salon et pour y jouer de la musique!"
+                );
+        }
 
-  const songInfo = await ytdl.getInfo(args[1]);
-  const song = {
-        title: songInfo.videoDetails.title,
-        url: songInfo.videoDetails.video_url,
-   };
+        const songInfo = await ytdl.getInfo(args[1]);
+        const song     = {
+                title: songInfo.videoDetails.title,
+                url  : songInfo.videoDetails.video_url,
+        };
 
-  if (!serverQueue) {
-    const queueContruct = {
-      textChannel: message.channel,
-      voiceChannel: voiceChannel,
-      connection: null,
-      songs: [],
-      volume: 5,
-      playing: true
-    };
-  
-	// On ajoute la queue du serveur dans la queue globale:
-	queue.set(message.guild.id, queueConstruct);
-	// On y ajoute la musique
-	queueContruct.songs.push(song);
- 
-    try {
-      var connection = await voiceChannel.join();
-      queueContruct.connection = connection;
-      play(message.guild, queueContruct.songs[0]);
-    } catch (err) {
-      console.log(err);
-      queue.delete(message.guild.id);
-      return message.channel.send(err);
-    }
-  } else {
-    serverQueue.songs.push(song);
-    return message.channel.send(`${song.title} has been added to the queue!`);
-  }
-}
+        if (!serverQueue) {
+                const queueConstruct = {
+                        textChannel : message.channel,
+                        voiceChannel: voiceChannel,
+                        connection  : null,
+                        songs       : [],
+                        volume      : 1,
+                        playing     : true,
+                };
 
-function play(guild, song) {
-  const serverQueue = queue.get(guild.id); // On récupère la queue de lecture
-  if (!song) { // Si la musique que l'utilisateur veux lancer n'existe pas on annule tout et on supprime la queue de lecture
-    serverQueue.voiceChannel.leave();
-    queue.delete(guild.id);
-    return;
-  }
+                // On ajoute la queue du serveur dans la queue globale:
+                queue.set(message.guild.id, queueConstruct);
+                // On y ajoute la musique
+                queueConstruct.songs.push(song);
 
-// On lance la musique 
-  const dispatcher = serverQueue.connection
-    .play(ytdl(song.url, { filter: 'audioonly' }))
-    .on("finish", () => { // On écoute l'événement de fin de musique
-      serverQueue.songs.shift(); // On passe à la musique suivante quand la courante se termine 
-      play(guild, serverQueue.songs[0]);
-    })
-    .on("error", error => console.error(error));
-  dispatcher.setVolume(serverQueue.volume); // On définie le volume
-  serverQueue.textChannel.send(`Démarrage de la musique: **${song.title}**`);
-}
+                try {
+                        // On connecte le bot au salon vocal et on sauvegarde l'objet connection
+                        var connection           = await voiceChannel.join();
+                        queueConstruct.connection = connection;
+                        // On lance la musique
+                        play(message.guild, queueConstruct.songs[0]);
+                }
+                catch (err) {
+                        //On affiche les messages d'erreur si le bot ne réussi pas à se connecter, on supprime également la queue de lecture
+                        console.log(err);
+                        queue.delete(message.guild.id);
+                        return message.channel.send(err);
+                }
+        }
+        else {
+                serverQueue.songs.push(song);
+                console.log(serverQueue.songs);
+                return message.channel.send(`${song.title} has been added to the queue!`);
+        }
 
-const queueConstruct = {
- textChannel: message.channel,
- voiceChannel: voiceChannel,
- connection: null,
- songs: [],
- volume: 1,
- playing: true,
-};
-
-
-try {
- // On connecte le bot au salon vocal et on sauvegarde l'objet connection
- var connection = voiceChannel.join();
- queueContruct.connection = connection;
- // On lance la musique
- play(message.guild, queueContruct.songs[0]);
-} catch (err) {
- //On affiche les messages d'erreur si le bot ne réussi pas à se connecter, on supprime également la queue de lecture
- console.log(err);
- queue.delete(message.guild.id);
- return message.channel.send(err);
 }
 
 function play(guild, song) {
@@ -148,24 +114,53 @@ function play(guild, song) {
 }
 
 function skip(message, serverQueue) {
-  if (!message.member.voice.channel) // on vérifie que l'utilisateur est bien dans un salon vocal pour skip
-    return message.channel.send(
-      "Vous devez être dans un salon vocal pour passer une musique!"
-    );
-  if (!serverQueue) // On vérifie si une musique est en cours
-    return message.channel.send("Aucune lecture de musique en cours !");
-  serverQueue.connection.dispatcher.end(); // On termine la musique courante, ce qui lance la suivante grâce à l'écoute d'événement finish
+        if (!message.member.voice.channel) // on vérifie que l'utilisateur est bien dans un salon vocal pour skip
+        {
+                return message.channel.send(
+                    "Vous devez être dans un salon vocal pour passer une musique!"
+                );
+        }
+        if (!serverQueue) // On vérifie si une musique est en cours
+        {
+                return message.channel.send("Aucune lecture de musique en cours !");
+        }
+        serverQueue.connection.dispatcher.end(); // On termine la musique courante, ce qui lance la suivante grâce à l'écoute d'événement
+                                                 // finish
 }
 
 function stop(message, serverQueue) {
- if (!message.member.voice.channel) // on vérifie que l'utilisateur est bien dans un salon vocal pour skip
-    return message.channel.send(
-      "Vous devez être dans un salon vocal pour stopper la lecture!"
-    );
-  if (!serverQueue) // On vérifie si une musique est en cours
-    return message.channel.send("Aucune lecture de musique en cours !");
-    serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
+        if (!message.member.voice.channel) // on vérifie que l'utilisateur est bien dans un salon vocal pour skip
+        {
+                return message.channel.send(
+                    "Vous devez être dans un salon vocal pour stopper la lecture!"
+                );
+        }
+        if (!serverQueue) // On vérifie si une musique est en cours
+        {
+                return message.channel.send("Aucune lecture de musique en cours !");
+        }
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end();
+}
+
+function play(guild, song) {
+        console.log(song);
+        const serverQueue = queue.get(guild.id); // On récupère la queue de lecture
+        if (!song) { // Si la musique que l'utilisateur veux lancer n'existe pas on annule tout et on supprime la queue de lecture
+                serverQueue.voiceChannel.leave();
+                queue.delete(guild.id);
+                return;
+        }
+        // On lance la musique
+        const dispatcher = serverQueue.connection
+            .play(ytdl(song.url, { filter: 'audioonly' }))
+            .on("finish", () => { // On écoute l'événement de fin de musique
+                    serverQueue.songs.shift(); // On passe à la musique suivante quand la courante se termine
+                    play(guild, serverQueue.songs[0]);
+            })
+            .on("error", error => console.error(error));
+        dispatcher.setVolume(1); // On définie le volume
+        serverQueue.textChannel.send(`Démarrage de la musique: **${song.title}**`);
 }
 
 client.on("message", message => {
